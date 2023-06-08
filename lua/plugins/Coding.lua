9e8a54b43c8942171e702926a51f1f5a5c799cbc
baigao-X <1007668733@qq.com>
2023-06-08 09:42:52 +0800

    style: 增加antovim的配置

--- lua/plugins/Coding.lua
+++ lua/plugins/Coding.lua
@@ -1,23 +1,23 @@
 return {
-    {
-        "theniceboy/antovim",
-        keys = {
-            { "<leader>an", "<Cmd>Antovim<CR>", desc = "Switch antonym" },
-        },
-        -- config = function()
-        --     vim.g.custom_antovim_definitions = [
-        --          ['this', 'that'],
-        --          ['yes', 'no', 'maybe'],
-        --          ['true', 'false'],
-        --          ['TRUE', 'FALSE'],
-        --          ['ON', 'OFF'],
-        --          ['on', 'off'],
-        --          ['OPEN', 'CLOSE'],
-        --          ['open', 'close'],
-        --          ['start', 'stop'],
-        --          ['STATIC', 'SHARED'],
-        --          ['==', '!=']
-        --     ]
-        -- end,
-    }
+	{
+		"theniceboy/antovim",
+		keys = {
+			{ "<leader>an", "<Cmd>Antovim<CR>", desc = "Switch antonym" },
+		},
+		config = function()
+			vim.g.custom_antovim_definitions = {
+				{ "this", "that" },
+				{ "yes", "no", "maybe" },
+				{ "true", "false" },
+				{ "TRUE", "FALSE" },
+				{ "ON", "OFF" },
+				{ "on", "off" },
+				{ "OPEN", "CLOSE" },
+				{ "open", "close" },
+				{ "start", "stop" },
+				{ "STATIC", "SHARED" },
+				{ "==", "!=" },
+			}
+		end,
+	},
 }
