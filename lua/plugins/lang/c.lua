ffac85933fb91fd6cbe40197b7764a2033d9b764
baigao-X <1007668733@qq.com>
2023-06-14 21:37:25 +0800

    fix: 修复language c 使用ccls的配置

---
+++ lua/plugins/lang/c.lua
@@ -0,0 +1,50 @@
+return {
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                ccls = {
+                    cmd = { "ccls" },
+                    filetypes = { "c", "cpp", "objc", "objcpp" },
+                    root_dir = function(fname)
+                        local util = require("lspconfig.util")
+                        local root_files = {
+                            "compile_commands.json",
+                            ".ccls",
+                            ".git",
+                            ".svn",
+                            ".root",
+                        }
+                        return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname)
+                    end,
+                    init_options = {
+                        compilationdatabasedirectory = "build",
+                        index = {
+                            threads = 0,
+                        },
+                        cache = {
+                            directory = "/tmp/ccls-cache",
+                        },
+                        clang = {
+                            excludeargs = { "-frounding-math" },
+                        },
+                    },
+                    settings = {
+                        semanticTokens = true,
+                    },
+                },
+            },
+            setup = {
+                ccls = function()
+                    -- workaround for gopls not supporting semantictokensprovider
+                    -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+                    require("lazyvim.util").on_attach(function(client, _)
+                        if client.name == "ccls" then
+                        end
+                    end)
+                    -- end workaround
+                end,
+            },
+        },
+    },
+}
