d5336d8e9a400f9b19acefa3fa6fdba514321178
baigao-X <1007668733@qq.com>
2023-06-15 13:02:28 +0800

    style: 关闭lua自动启动lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,9 +2,29 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		---@class PluginLspOpts
-		opts = {
-			-- Automatically format on save
-			autoformat = false,
-		},
-	},
+        opts = {
+            -- Automatically format on save
+            autoformat = false,
+
+            servers = {
+                jsonls = {
+                    autostart = false,
+                },
+                lua_ls = {
+                    autostart = false,
+                    -- mason = false, -- set to false if you don't want this server to be installed with mason
+                    settings = {
+                        Lua = {
+                            workspace = {
+                                checkThirdParty = false,
+                            },
+                            completion = {
+                                callSnippet = "Replace",
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    },
 }
