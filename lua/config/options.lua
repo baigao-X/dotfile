0b52c7910db2c561df5f4e7a8eb2748c1dc4410e
baigao-X <1007668733@qq.com>
2023-06-19 23:39:29 +0800

    style: options settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,5 +6,11 @@
 --
 local opt = vim.opt
 
-opt.tabstop = 4    -- Number of spaces tabs count for
-opt.shiftwidth = 4 -- Size of an indent
+opt.tabstop = 4                                       -- Number of spaces tabs count for
+opt.shiftwidth = 4                                    -- Size of an indent
+opt.clipboard:append {"unnamedplus"}                  -- 设置vim复制到系统剪贴板，默认复制到vim内部的缓冲区
+opt.wrap = true                                       -- 自动换行
+opt.textwidth = 120                                   -- 设置一行最大显示字符
+-- opt.colorcolumn = 120
+
+
