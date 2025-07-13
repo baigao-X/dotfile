26e455b5c36c44dad927e5bdc66e5279bb40b390
baigao-X <1007668733@qq.com>
2025-07-13 13:33:57 +0800

    feat: 修改nvim 系统剪贴板配置

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -22,3 +22,13 @@ end
 map("i", "jk", "<ESC>", { desc = "ESC" })
 map("n", "<Leader>q", ":q<cr>", { desc = "退出当前窗口" })
 map("n", "<Leader>w", ":w<cr>", { desc = "保存当前窗口" })
+
+-- 系统剪贴板复制快捷键
+map("v", "<C-S-c>", function()
+  vim.cmd('normal! "+y')
+  vim.notify("已复制到系统剪贴板")
+end, { desc = "复制到系统剪贴板" })
+map("n", "<C-S-c>", function()
+  vim.cmd('normal! "+yy')
+  vim.notify("已复制整行到系统剪贴板")
+end, { desc = "复制整行到系统剪贴板" })
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -8,7 +8,6 @@ local opt = vim.opt
 
 opt.tabstop = 4                                       -- Number of spaces tabs count for
 opt.shiftwidth = 4                                    -- Size of an indent
-opt.clipboard:append {"unnamedplus"}                  -- 设置vim复制到系统剪贴板，默认复制到vim内部的缓冲区
 opt.wrap = true                                       -- 自动换行
 opt.textwidth = 120                                   -- 设置一行最大显示字符
 -- opt.colorcolumn = 120
@@ -21,3 +20,25 @@ opt.eof = false
 opt.endofline = false
 opt.binary = false
 
+vim.g.autoformat = false
+
+vim.opt.list = true
+vim.opt.listchars = {tab = ">-", space = "."}
+
+-- 剪贴板配置
+vim.opt.clipboard = "unnamedplus"
+-- 确保使用正确的剪贴板提供者
+if vim.fn.has('unix') == 1 then
+  vim.g.clipboard = {
+    name = 'xclip',
+    copy = {
+      ['+'] = 'xclip -selection clipboard',
+      ['*'] = 'xclip -selection primary',
+    },
+    paste = {
+      ['+'] = 'xclip -selection clipboard -o',
+      ['*'] = 'xclip -selection primary -o',
+    },
+    cache_enabled = 1,
+  }
+end
